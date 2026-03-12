803e6524d4716fab0d3cd7c01a086302e7530e29
Romeo Hazel <romeobrwn06@gmail.com>
2026-03-12 01:09:40 -0700

    add Vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { AmbientProvider } from "@/hooks/useAmbient";
 
 export const metadata: Metadata = {
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body>
         <AmbientProvider>{children}</AmbientProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
